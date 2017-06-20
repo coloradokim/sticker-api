8f9375b7cc96182639d52080e01b2b7761d021ca
coloradokim <schlesinger.kim@gmail.com>
2017-06-20 15:15:01 -0600

    add comma

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/kims-web-store'
-  }
+  },
   production {
     client: 'pg',
-    connection: process.env.DATABASE_URL 
+    connection: process.env.DATABASE_URL
   }
 };
